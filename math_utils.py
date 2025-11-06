a41b6f2cd9d2667f9a2b2cc5597417f27073809a
noamsalo <noamsal30@gmail.com>
2025-11-06 08:32:26 +0200

    Fix indentation and whitespace in math_utils.py
    Fix indentation issues and clean up whitespace in math_utils.py.

--- math_utils.py
+++ math_utils.py
@@ -5,11 +5,9 @@ def find_max_number(num1, num2, num3):
         return num2
       else:
         return num3
-
 def find_mean(num1, num2, num3):
      find_mean = (num1 + num2 + num3) / 3
     return find_mean
-
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
      std =  (( num1 - mean)**2 + ( num2 - mean )**2 + (num3 - mean)**2 ) / 3
